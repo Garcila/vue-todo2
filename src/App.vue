55439e43b4fce5abf728c30f5a69a04d9a2ff96f
German Arcila <germanarcila@gmail.com>
2019-07-16 15:27:06 -0400

    first commit, did git fix audit --force to try and fix vulnerabilities'

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="container">
+    <h1>Stuff I Want To DO 🤡</h1>
+    <input type="text" v-model="newTodo" />
+    <h2>{{newTodo}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      newTodo: ""
+    };
+  }
+};
+</script>
+
+<style>
+</style>
