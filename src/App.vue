828bd0818babdea5e053c486a3dd981afa983c2c
German Arcila <germanarcila@gmail.com>
2019-07-17 11:46:03 -0400

    all functionality is there

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app" class="container">
-    <h1>Stuff I Want To DO 🤡</h1>
-    <input type="text" v-model="newTodo" />
-    <h2>{{newTodo}}</h2>
+    <h1 style="text-align: center; margin: 2rem;">Stuff I Want To DO 🤡</h1>
+    <form action="#" @submit.prevent="addTodo">
+      <input type="text" v-model="newTodo" placeholder="Enter new todo 🤡" class="form-control" />
+
+      <button class="btn btn-primary mt-3">Add Todo</button>
+    </form>
+    <ul id="example-1" class="list-group mt-3">
+      <li
+        class="list-group-item"
+        v-for="todo in todos"
+        v-bind:key="todo.id"
+        v-on:click="toggleTodo(todo)"
+        v-bind:class="{ completed: todo.done }"
+      >
+        <div class="list-item">
+          {{ todo.title }}
+          <button class="li-delete-button" v-on:click="deleteTodo(todo)">X</button>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -11,11 +28,42 @@ export default {
   name: "app",
   data() {
     return {
-      newTodo: ""
+      newTodo: "",
+      todos: [],
+      keyCounter: 0
     };
+  },
+  methods: {
+    addTodo() {
+      const todo = { title: this.newTodo, done: false, id: this.keyCounter };
+      this.todos.push(todo);
+      this.keyCounter = this.keyCounter + 1;
+      this.newTodo = "";
+    },
+    toggleTodo(todo) {
+      todo.done = !todo.done;
+    },
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    }
   }
 };
 </script>
 
 <style>
+.completed {
+  text-decoration: line-through;
+  background-color: #f2efed;
+}
+.list-item {
+  display: flex;
+  justify-content: space-between;
+}
+.li-delete-button {
+  border: none;
+  border-radius: 3px;
+  background-color: rgb(255, 86, 86);
+  color: white;
+}
 </style>
